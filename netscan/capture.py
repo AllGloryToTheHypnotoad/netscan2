401c473bdb86861e423174bba9aded1abb24f2be
walchko <kevin.walchko@outlook.com>
2016-01-28 18:15:27 -0700

    capture update

--- netscan/capture.py
+++ netscan/capture.py
@@ -5,7 +5,10 @@ import os
 import sys
 
 class CapturePackets(object):
-    def __init__(self,iface='en1',filename='test.pcap',num_packets=300):
+    """
+    todo
+    """
+    def __init__(self,iface,filename='test.pcap',filter='',num_packets=300):
         # list all the network devices
         #print pcapy.findalldevs()
 
@@ -13,7 +16,7 @@ class CapturePackets(object):
         promiscuous = False
         read_timeout = 100 # in milliseconds
         pc = pcapy.open_live(iface, max_bytes, promiscuous, read_timeout)
-        # pc.setfilter('tcp')
+        if filter: pc.setfilter(filter)
         self.dumper = pc.dump_open(filename)
         pc.loop(num_packets, self.recv_pkts) # capture packets
 
@@ -22,37 +25,38 @@ class CapturePackets(object):
         try:
             # print data
             self.dumper.dump(hdr, data)
-        except KeyboardInterrupt:
+        except KeyboardInterrupt: # probably show throw error instead
             exit('keyboard exit')
         except:
             exit('crap ... something went wrong')
-        # pc.loop(num_packets, self.recv_pkts)
-
-
 
 def handleArgs():
 	description = """
-    Grabs packets from an interface and writes them to a file.
+    Grabs packets from an interface (default: en1) and writes them to a file
+    (default: network.pcap).
 
 	example:
 
-		getpackets -f filename -i iface
+		capture -s filename -i iface -f 'tcp'
 	"""
 	parser = argparse.ArgumentParser(description)
-	parser.add_argument('ip', help='ip address to get hostname for') # mandatory arg
-	args = parser.parse_args()
+    	parser.add_argument('-i', '--interface', help='network interface to use', default='en1')
+        	parser.add_argument('-f', '--filter', help='filters to pass to libpcap', default='')
+    # 	parser.add_argument('-d', '--display', help='print to screen', action='store_true', default=False)
+    	parser.add_argument('-s', '--save', help='save output to a file', default='network.pcap')
+	args = vars(parser.parse_args())
 	return args
 
 def main():
 	# handle inputs
-	# args = handleArgs()
+	args = handleArgs()
 
 	# check for sudo/root privileges ??
 	if os.geteuid() != 0:
 		exit('You need to be root/sudo ... exiting')
 
 	try:
-		CapturePackets('en1')
+		CapturePackets(args['interface'],args['save'],args['filter'])
 	except KeyboardInterrupt:
 		exit('You hit ^C, exiting ... bye')
 	# except:
