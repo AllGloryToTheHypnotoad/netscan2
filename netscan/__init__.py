cb65ec3edbd784c53e2bcfc01a7dd9a6bc93e9ca
walchko <walchko@users.noreply.github.com>
2018-05-28 22:24:06 -0600

    reorg

--- netscan/__init__.py
+++ netscan/__init__.py
@@ -1,10 +1,9 @@
+from netscan.ActiveScan import ArpScan, IP, Pinger, PortScanner, ActiveMapper
+from netscan.PassiveScan import PassiveMapper
+from netscan.lib import MacLookup
+from netscan.lib import WhoIs
+from netscan.lib import GetHostName
+
+__author__ = 'Kevin Walchko'
+__license__ = 'MIT'
 __version__ = '2.1.4'
-
-# this automatically loads these into name space, better to manually load them
-# __all__ = ["NetworkScan",
-#           "WebPage",
-#           "YamlDoc",
-#           "IP"]
-
-import ActiveScan
-import PassiveScan
--- setup.py
+++ setup.py
@@ -1,78 +1,51 @@
 from setuptools import setup
 import os
 from netscan import __version__ as VERSION
-from setuptools.command.test import test as TestCommand
+from build_utils import BuildCommand
+from build_utils import PublishCommand
+from build_utils import BinaryDistribution
 
 
-class PublishCommand(TestCommand):
-	def run_tests(self):
-		print('Publishing to PyPi ...')
-		os.system("python setup.py bdist_wheel")
-		os.system("twine upload dist/netscan-{}*.whl".format(VERSION))
+PACKAGE_NAME = 'netscan'
+BuildCommand.pkg = PACKAGE_NAME
+PublishCommand.pkg = PACKAGE_NAME
+PublishCommand.version = VERSION
 
 
 setup(
 	name='netscan',
 	version=VERSION,
-
 	description='A simple Python active and passive network scanner for linux and OSX',
 	long_description=open('README.rst').read(),
 	keywords='network scanner active passive dpkt html',
-
-	# The project's main homepage.
 	url='https://github.com/walchko/netscan2',
-
-	# Author details
 	author='Kevin Walchko',
-	author_email='kevin.walchko@outlook.com',
-
-	# Choose your license
+	author_email='walchko@users.noreply.github.com',
 	license='MIT',
-
-	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
 	classifiers=[
-		# How mature is this project? Common values are
-		#   3 - Alpha
-		#   4 - Beta
-		#   5 - Production/Stable
 		'Development Status :: 4 - Beta',
-
-		# Indicate who your project is intended for
-		#'Intended Audience :: Developers',
-		#'Topic :: Software Development :: Build Tools',
-
-		# Pick your license as you wish (should match "license" above)
 		'License :: OSI Approved :: MIT License',
-
-		# Specify the Python versions you support here. In particular, ensure
-		# that you indicate whether you support Python 2, Python 3 or both.
 		'Programming Language :: Python :: 2.7',
-
-		# Operating systems this runs on
 		'Operating System :: Unix',
 		'Operating System :: MacOS :: MacOS X',
 		'Operating System :: POSIX',
-
-		# what does this do?
 		'Topic :: Utilities',
 		'Topic :: System :: Shells',
 		'Environment :: Console'
 	],
 	cmdclass={
-		# 'test': NoseTestCommand,
 		'publish': PublishCommand,
-		# 'tag': GitTagCommand,
-		# 'clean': CleanCommand
+		'make': BuildCommand
 	},
 	packages=['netscan'],
 	install_requires=['requests', 'pcapy', 'dpkt', 'netaddr'],
-	scripts=[
-		'bin/ascan.py',
-		'bin/pscan.py',
-		'bin/gethostname.py',
-		'bin/getvendor.py',
-		'bin/ipwhois.py'
-	]
+	# scripts=[
+	# 	'bin/ascan.py',
+	# 	'bin/pscan.py',
+	# 	'bin/gethostname.py',
+	# 	'bin/getvendor.py',
+	# 	'bin/ipwhois.py'
+	# ]
 	# entry_points={
 	# 	'console_scripts': [
 	# 		'netscan=netscan.netscan:main',
